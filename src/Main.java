d194a631ca273265f1c5103b98797cf207901e85
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 <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>
2024-08-30 13:58:45 +0000

    First version

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+       int a = 12;
+       int b = 22;
+       System.out.println(a + b);
+       System.out.println("Hello coi!");
+    }   
+}
---
+++ src/Outcome.java
@@ -0,0 +1,9 @@
+public class Outcome {
+   String name;
+   int odds;
+
+   Outcome(String name, int odds) {
+       this.name = name;
+       this.odds = odds;
+   }
+}
