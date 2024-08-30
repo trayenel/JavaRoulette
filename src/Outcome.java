3ee382892b8406bdc47e360a8dc6a2269051ec84
trayenel <agent_tygae@yahoo.com>
2024-08-30 14:24:17 +0000

    Added getAmount method in Outcome class.

--- src/Outcome.java
+++ src/Outcome.java
@@ -1,9 +1,13 @@
 public class Outcome {
-   String name;
-   int odds;
+    private String name;
+    private int odds;
 
    Outcome(String name, int odds) {
        this.name = name;
        this.odds = odds;
+   }
+
+   private float winAmount(float amount) {
+       return this.odds * amount;
    }
 }
